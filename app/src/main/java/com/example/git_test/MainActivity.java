5923a622d4400904b1a001c86bb3b5aaa67f3735
lhw <2572931287@qq.com>
2020-12-15 15:07:59 +0800

    增加了一个按钮和跳转界面

--- app/src/main/java/com/example/git_test/MainActivity.java
+++ app/src/main/java/com/example/git_test/MainActivity.java
@@ -2,14 +2,25 @@ package com.example.git_test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    //jklj
+    private Button mBtn_bt1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBtn_bt1 = findViewById(R.id.btn_bt1);
+        mBtn_bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(MainActivity.this,Json.class);
+                startActivity(intent);
+            }
+        });
     }
 }
